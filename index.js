ff3433d8a8e6c89e8516b933de5a0ed90eb2dd79
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 08:12:32 +0600

    modgoDB configaration added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 7000 ;
 
@@ -11,12 +13,44 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.evuna6q.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
 // Home Route 
 
 app.get( '/' , (req, res) => {
     res.send('Welcome to Royal Espresso Server')
 })
 
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  
+  finally {
+    // Ensures that the client will close when you finish/error
+   /*  await client.close(); */
+  }
+}
+run().catch(console.dir);
 
 
 
