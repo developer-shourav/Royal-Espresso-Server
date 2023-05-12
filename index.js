b172ca9e1f293d41e7fb6c976b8c77262e70682d
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-12 06:41:37 +0600

    server primary setup complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 7000 ;
+
+
+
+
+// Middleware 
+app.use(cors())
+app.use(express.json())
+
+
+// Home Route 
+
+app.get( '/' , (req, res) => {
+    res.send('Welcome to Royal Espresso Server')
+})
+
+
+
+
+
+
+// Must have 
+app.listen( port , () => {
+    console.log(`Our Coffee shop server is Running on the PORT: ${port}`);
+})
